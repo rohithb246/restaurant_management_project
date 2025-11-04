e95fdfeb53492626062dd8f19c2386a03fd75314
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-04 21:39:34 +0530

    58378

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Coupon(models,Model):
+      code = models.CharField(max_length=50, unique=True)
+      discount_percentage = models.DecimalField(max_digit= 5, decimal_places=2)
+      is_active = models.BooleanField(default=True)
+      valid_from = models.DateField()
+      valid_until = models.DateField()
+
+      def _str_(self):
+        return self.code
+        
