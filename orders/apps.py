eb88bf3d0ab3b6974eba93e14a08d72ca786a4e7
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-04 22:29:07 +0530

    58377

--- orders/apps.py
+++ orders/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+    def ready(self):
+        from .models import OrderStatus
+        from . import PENDING,PROCESSING,COMPLETE,CANCELLED
+
+        default_statuses = [PENDING,PROCESSING,COMPLETE,CANCELLED]
+    
+    for status in default_statuses:
+        OrderStatus.objects.get_or_create(name=status)
