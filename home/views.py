dafe9c28b2065af5ca85ac560472f926da989fd7
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-04 23:12:56 +0530

    58379

--- home/views.py
+++ home/views.py
@@ -3,11 +3,6 @@ from .models import Item
 from .serializers import ItemSerializer
 
 # List and Create API for all items
-class ItemListCreateView(generics.ListCreateAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-
-# Retrieve, Update, and Delete API for a single item
-class ItemDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
+class MenuCategoryListView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
