5dfd112dea4c75645cdebc18d51c0e6ed827e979
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-04 22:59:09 +0530

    58375

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class OrderStatus(models.Models):
-    name = models.CharField(Max_length=50, unique=True)
+class MenuCategory(models.Models):
+    name = models.CharField(Max_length=100, unique=True)
 
     def _str_(self):
         return self.name
