caed48b681d8714cc5aeb40dc85363fa3360b5a8
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-04 23:14:38 +0530

    58379

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import ItemListCreateView, ItemDetailView
 
 urlpatterns = [
-    path('items/', ItemListCreateView.as_view(), name='item-list'),
-    path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
+   path('categories/', MenuCategoryListView.as_view(),name='menu-categories'),
 ]
