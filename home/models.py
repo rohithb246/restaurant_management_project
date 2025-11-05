89c16a0dc89426c4d461a9a64f3606fbb54dc0ef
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-05 12:38:31 +0530

    58455

--- home/models.py
+++ home/models.py
@@ -3,5 +3,14 @@ from django.db import models
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+class MenuItem(models.Model):
+    name = models.CharField(max_lenght=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
+
+    is_featured = models.BooleanField(default=false)
+
     def _str_(self):
         return self.name
