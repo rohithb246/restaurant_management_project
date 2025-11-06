3330286400441fd96e332273bf742cd46f1f59b8
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-06 12:57:53 +0530

    58476

--- home/models.py
+++ home/models.py
@@ -3,22 +3,13 @@ from django.db import models
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
-class MenuItem(models.Model):
-    name = models.CharField(max_lenght=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
-    is_available = models.BooleanField(default=True)
-
-    is_featured = models.BooleanField(default=false)
+class NutritionalInformation(models.Model):
+    menu_item = models.ForeignKey('MenuItem', on_delete=models.CASCADE, related_name='nutritional_info')
+    calories = models.IntegerField()
+    protein_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5, decimal_places=2)
 
 
-class Restaurant(models,Model):
-    name = models.CharField(max_length=255)
-    address = models.TextField()
-    phone = models.CharField(max_length=20)
-
-    has_delivery = models.BooleanField(default=False)
-
-def _str_(self):
+    def _str_(self):
         return self.name
