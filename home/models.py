b1c95371f825bb4ce419f5cc376ae5a05e79cd63
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-06 21:40:27 +0530

    58472

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
 class MenuItem(models.Model):
-    name = models.CharField(max_lenght=255)
+    name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
@@ -32,5 +32,5 @@ class DailySpecial(models.Model):
             return specials.order_by(?).first()
         return None
         
-def _str_(self):
+def __str__(self):
         return self.name
