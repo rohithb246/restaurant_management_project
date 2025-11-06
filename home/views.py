d158a5587eaf99810ce0c18ff7e2d2849c20c57e
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-06 13:30:14 +0530

    58475

--- home/views.py
+++ home/views.py
@@ -1,8 +1,28 @@
+from rest_framework.views import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q
 from rest_framework import generics
-from .models import Item
-from .serializers import ItemSerializer
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # List and Create API for all items
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+
+class MenuItemSearchViewSet(ViewSet):
+    pagination_class = MenuItemSearchPagination
+
+    def list(self, request):
+        query = response.GET.get('q', '')
+        queryset = MenuItem.objects.all()
+
+        if query:
+            queryset = queryset.filter(Q(name_icontains=query))
+
+        pagination = self.pagination_class()
+        result_page = pagination.paginate_queryset(queryset, request)
+        serializer = MenuItemSerializer(result_page, many=true)
+        return paginator.get_paginated_response(serializer.data)
