6da689d705c5a155ceb4fa0726223ab6dd50bef5
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-06 13:33:05 +0530

    58475

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import ItemListCreateView, ItemDetailView
+from .views import MenuItemSearchSet
+
+urlpatterns = [
+    path('menu-items/search/', MenuItemSearchSet.as_view({'get':'list'}),name='menu-items-search'),
+]
 
 urlpatterns = [
    path('categories/', MenuCategoryListView.as_view(),name='menu-categories'),
