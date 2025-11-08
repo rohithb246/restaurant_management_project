306c4740c8b6439bfa0224d399364b9015c2b71a
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-08 21:36:19 +0530

    58595

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import MenuItem
+from .models import Ingredient
 
 class MenuIteSerializer(serializers.modelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', 'name','description', 'price' ]
+
+class IngredientSerializer(serializers.modelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
