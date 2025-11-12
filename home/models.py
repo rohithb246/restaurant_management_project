404daa1ea672f72f8f582f9eb95743bae4bbfb4c
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-12 22:25:01 +0530

    60335

--- home/models.py
+++ home/models.py
@@ -42,3 +42,19 @@ class MenuItem(models.Model):
 
     def _str_(self):
         return self.name
+
+class Category(models.Model):
+    Category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Category_name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items')
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self)
+    return self.name
