a4c3497b2d92a5a1b92a52905c0bf7bc3eece6b1
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-12 21:52:52 +0530

    60337

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.models import User
 from django.db import models
 from .models import OrderStatus
 
+class OrderStatus(models.Model):
+    name = models.charField(max_length=50)
 
+    def __str__(self):
+        return self.name
+ 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
     status = models.ForeignKey(
         'OrderStatus',
         on_delete=models.SET_NULL,
@@ -15,5 +16,5 @@ class Order(models.Model):
     )
 
     def _str_(self):
-        return f"Order #{self.id}"
+        return f"Order #{self.id} - {self.status.name if self.status else 'No status'}"
         
