8743ad6b9eb5a89063646db5c273322244224662
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-12 22:12:14 +0530

    60336

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import path
+from . import views
 
-# Create your tests here.
+urlspattern = [
+    path('api/table/', views.TableListView.as_view(), name'table-list'),
+
+    path('api/table/<int::pk>/', views.TableDetailView.as_view(), name='table-detais'),
+]
