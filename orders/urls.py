0bf5c4a7b085d7f4c4683e27fa9e494875b4c6d4
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-13 16:11:58 +0530

    60507

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import *
+from .view import OrderDetailAPIView
 
 urlpatterns = [
     
+]
+
+urlpatterns = [
+    path('<int:id>/'OrderDetailAPIView.as_view(), name='order_detail_api')
 ]
