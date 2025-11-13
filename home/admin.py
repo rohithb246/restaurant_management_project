e65a4f41f4b1aeccb588b1f5297c37ce5486f515
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-13 15:41:10 +0530

    60509

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from .models import Restaurant
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ("name", "phone", "has_delivery")
     list_editable = ("has_delivery",)
+    search_fields = ("name", "address")
+    list_filter = ("is_active",)
