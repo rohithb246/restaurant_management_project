3ffb3445a1817eb550b18ec6a1fa8c4d608f5d4a
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-16 19:12:28 +0530

    60663

--- home/models.py
+++ home/models.py
@@ -58,3 +58,12 @@ class MenuItem(models.Model):
 
     def __str__(self)
     return self.name
+
+class LoyaltyProgram(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    points_required = models.IntegerField(unique=True)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
