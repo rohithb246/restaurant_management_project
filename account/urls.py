5136dfd8d53c7073db7e1ef2e2f64729b26560c7
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-16 20:12:54 +0530

    60660

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import *
+from .views import UserProfileViewSet
+
+user_profile = UserProfileViewSet.as_view({
+    'put': 'update'
+})
 
 urlpatterns = [
+    path('profile/update/', user_profile, name='profile-update'),
     
 ]
