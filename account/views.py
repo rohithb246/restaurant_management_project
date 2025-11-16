ba6b658fcd12915d5ee1f6de86d96129461750b3
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-16 20:12:40 +0530

    60660

--- account/views.py
+++ account/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenicated
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserUpdateSerializer
+
+class  UserProfileViewSet(viewsets.Viewset):
+    permission_classes = [IsAuthenicated]
+
+    def update(self, request):
+        user = request.User
+        serializer = UserUpdateSerializer(user, data=request.data)
+
+        if serializer.is_vaild():
+            serializer.save()
+            return Response({
+                "message": "profile updated successfully"
+                "data": serializer.data
+            }), status=status.HTTP_200_OK
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
