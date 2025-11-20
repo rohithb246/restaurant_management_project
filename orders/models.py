9cb6546c2d116ec1ad80103cf572736f11e1aa91
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-20 23:07:40 +0530

    60860

--- orders/models.py
+++ orders/models.py
@@ -9,7 +9,15 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
- 
+
+class PaymentMethod(models.Model):
+    name = models.charField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+         
 class Order(models.Model):
     customer = models.ForeignKey(customer, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
