08d8485342b9373a01ae7770fba38ebbf7fb3a39
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-20 23:41:25 +0530

    60858

--- orders/utils.py
+++ orders/utils.py
@@ -15,6 +15,15 @@ while True:
     if not Coupon.objects.filter(code-code).exists():
         return code
 
+def generate_unique_order_id(length=8):
+    characters = string.ascii_uppercase + string.digits
+
+    while True:
+        order_id = ''.join(secrets.choice(characters) for _ in range(length))
+
+        if not Order.objects.filter(order_id=order_id).exist():
+            return order_id
+
 def get_daily_sales_total(date):
     sales_data = Order.objects.filter(created_at_date=date).aggregate(total_sum=Sum('total_price'))
     total = sales_data['total_sum']
