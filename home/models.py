4ff53a90c378edc644be4b96b8b550b906199cbf
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-20 23:22:00 +0530

    60859

--- home/models.py
+++ home/models.py
@@ -79,3 +79,11 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class ContactFormSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
