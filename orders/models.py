6bf0c865635231aa8d0c5c9789f6134ec85d4d12
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-20 23:45:13 +0530

    60858

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from .models import OrderStatus
 from decimal import Decimal
 from account.models import Customer
 from home.models import MenuItem
+from .utils import generate_unique_order_id
 
 class OrderStatus(models.Model):
     name = models.charField(max_length=50)
@@ -55,4 +56,16 @@ class OrderItem(models.model):
 
     def __str__(self):
         return f"{self.menu_item.name} x {self.quantity}"
+
+class order(model.Model):
+    order_id = models.charField(max_length=20, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.order_id:
+            self.order_id = generate_unique_order_id()
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.order_id
         
