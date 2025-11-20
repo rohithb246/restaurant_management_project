40ac1243720b7255c5c1696485b7add9d3abccdd
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-20 23:37:09 +0530

    60859

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from .views import ItemListCreateView, ItemDetailView
 from .views import MenuItemSearchSet
 from .views import MenuItemIngredientsView
 from .views import AvailableTablesAPIView
+from .views import ContactFormSubmission
 
 urlpatterns = [
     path('menu-items/search/', MenuItemSearchSet.as_view({'get':'list'}),name='menu-items-search'),
@@ -18,4 +19,8 @@ urlpatterns = [
 
 urlpatterns = [
     path('api/tables/available/',AvailableTablesAPIView.as_view(), name='available_tables_api')
+]
+
+urlpatterns = [
+    path('contact/submit/', ContactFormSubmissionView.as_view(), name='contact-submit'),
 ]
