b7289b1f01991ab5c17a0893b81244212f60862d
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-23 21:40:52 +0530

    60968

--- orders/views.py
+++ orders/views.py
@@ -7,3 +7,27 @@ class OrderDetailAPIView(RetrieveAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     lookup_field = 'id'
+
+    @action(detail=True, methods=['delete'], url_path="cancel")
+    def cancel_order(self, request, pk=None):
+        try:
+            order = Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            return Respose(
+                {"error": "order not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        if request.user.customer != order.customer:
+            return Respose(
+                {"error": "you are allowed to cancel this order."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        
+        order.status = 'Cancelled'
+        order.save()
+
+        return Respose(
+            {"message": "Order successfully cancelled."},
+            status=status,HTTP_200_OK
+        )
