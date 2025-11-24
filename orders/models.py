67fd1b2047ca0f91f35b01e2442f63647c55b8e5
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-24 18:28:45 +0530

    60966

--- orders/models.py
+++ orders/models.py
@@ -20,6 +20,7 @@ class PaymentMethod(models.Model):
         return self.name
          
 class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     customer = models.ForeignKey(customer, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
