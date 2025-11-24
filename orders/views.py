7d040e3122d5db28a81bf4cc83e18a65a72371b3
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-24 18:47:07 +0530

    60966

--- orders/views.py
+++ orders/views.py
@@ -38,3 +38,22 @@ class PaymentMethodListView(ListAPIView):
 
     def get_queryset(self):
         return PaymentMethod.objects.filter(is_active=True)
+
+class UpdateOrderStatusView(APIView):
+    def put(self, request, order_id):
+        try:
+            order = Order,objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Respose(
+                 {"error": "Order not found"},
+                 status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = OrderStatusUpdateSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.update(order, serializer.validated_data)
+            return Respose(
+                {"message": "order status updated successfully"},
+                status=status.HTTP_200_OK
+            )
+            return Respose(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
