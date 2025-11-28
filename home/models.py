dfd05abace70e0eb775afac7478c2a247721d0a0
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-28 19:35:31 +0530

    61264

--- home/models.py
+++ home/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from .models import MenuItem
+
+class UserReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    MenuItem = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True, null=True)
+    review_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'menu_item')
+        ordering = ['-review_date']
+
+        def __str__(self):
+            return f"{self.user.username} - {self.menu_item.name} ({self.rating}/5)"
 
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
