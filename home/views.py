13e2cc479c6047d80a23cd349d09b932dbc79dd3
rohithb246 <121705447+rohithb246@users.noreply.github.com>
2025-11-28 19:48:23 +0530

    61263

--- home/views.py
+++ home/views.py
@@ -1,8 +1,24 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Item
 from .serializers import ItemSerializer
 
-# List and Create API for all items
+class CreateUserReview(generics.CreateAPIView):
+    serializer_class = UserReviewSerializer
+    permission_classes = [permissions.IsAutenticated]
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+
+class MenuItemReviewListView(generics.ListAPIView):
+    serializer_class = UserReviewSerializer
+
+    def get_queryset(self):
+        menu_item_id = self.kwargs.get("menu_item_id")
+        return UserReview.objects.filter(menu_item_id=menu_item_id)
+        
